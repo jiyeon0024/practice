2537f1a95ed5978cb115e4846fc84249bb5c72fc
jiyeon park <jiyeonpark0024@gmail.com>
2022-11-18 23:03:36 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,47 @@
+
+
+//Select DOM
+const Name = document.querySelector(".title");
+const addList = document.querySelector(".addList");
+const AddBtn = document.querySelector(".submitBtn");
+const todoFilter = document.querySelector(".todo-filter");
+
+
+//Event Listeners
+Name.addEventListener("load",getName);
+AddBtn.addEventListener("click",addTodo);
+// addList.addEventListener("click",Deltodo);
+// todoFilter.addEventListener("click",filter);
+
+
+//Functions
+    function getName(){
+    
+
+    let n = prompt("Enter your name");
+    if(n=""){
+        Name.innerHTML=`${n}'s Todo List`;
+    }else{
+        
+        alert("Please enter your name");
+    
+    console.log(`user_name=${n}`);
+}
+    }
+getName(); 
+
+
+function addTodo(e){
+    
+    
+    e.preventDefault();
+
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+
+    const todo_List = document.createElement("li");
+    todo_List.classList.add("li");
+
+    todoDiv.appendChild(todo_List);
+
+}
